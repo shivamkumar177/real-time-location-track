905701d91e3897e50aeda12aebe2cb58c5d66769
Shivam Kumar <shivamk1717@gmail.com>
2024-07-05 01:01:26 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app)
+
+const io = socketio(server)
+app.get("/", function(req, res){
+    res.send("hey there")
+});
+ 
+server.listen(3000);
